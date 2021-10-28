11b5a3e54b1b133157f47e0920d33585e2b5917c
zhouzijie <zhouzijie@didiglobal.com>
2021-10-28 20:20:04 +0800

    [add] 添加Gee代码

---
+++ src/gee/gee.go
@@ -0,0 +1,55 @@
+package gee
+
+import (
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(*Context)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router *router
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handle HandlerFunc) {
+	engine.addRoute("POST", pattern, handle)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := NewContext(w, req)
+	engine.router.handle(c)
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share a Engine instance
+}
+
+type Engine struct {
+	*RouterGroup
+	router *router
+	groups []*RouterGroup // store all groups
+}
